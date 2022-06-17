a132670ae55970632142498aca1e660abc4f502c
calhep <callumahepworth@gmail.com>
2022-06-16 19:34:07 -0700

    Added project loader and starter images.

--- website/src/components/projects.js
+++ website/src/components/projects.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-export default function Projects() {
+import project_data from '../project_data.json'
+import '../projects.css'
+
+
+function Projects() {
+
+    const parseImg = (img) => {
+        return "images/" + img + ".png";
+    }
+
     return (
-        <div>
-            Projects
+        <div className="project-section">
+            {project_data.map((data) => (
+                <div key={data.id}>
+                    <div className="project">
+                        <div className="project-text">
+                            <h3>{data.title}</h3>
+                            <p>{data.body}</p>
+                        </div>
+                        <div className="project-image">
+                            <img src={parseImg(data.image)} alt={data.image} />
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+            ))}
         </div>
     );
 }
+
+export default Projects;
