23461aee00f0cc3cb42491b94d3d0619525eda65
calhep <callumahepworth@gmail.com>
2022-06-24 02:26:40 -0700

    Revert "Updates"
    This reverts commit c481ee7242b02caa804f1322574af80ebaaacffd.

---
+++ src/components/footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+import '../App.js'
+import '../footer.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <ul>
+                    <li>
+                        <a href="https://github.com/calhep">
+                            <FontAwesomeIcon icon={faGithub} size='xl' className="fa-icon" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/callumhepworth/">
+                            <FontAwesomeIcon icon={faLinkedin} size='xl' className="fa-icon" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:callumahepworth@gmail.com">
+                            <FontAwesomeIcon icon={faSquareEnvelope} size='xl' className="fa-icon" />
+                        </a>
+                    </li>
+                </ul>
+                <div id='copyright'>
+                    Built by Callum H. using React.
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/components/projects.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import project_data from '../project_data.json'
+import '../projects.css'
+
+function Media(props) {
+
+    if (props.media === "video") {
+        return (
+            <video width="200px" height="auto" controls>
+                <source src={"videos/" + props.filename + ".mp4"} type="video/mp4" />
+            </video>
+        );
+    };
+    if (props.media === "image") {
+        return (
+            <img src={"images/" + props.filename + ".png"} alt={props.filename} />
+        );
+    };
+}
+
+function ProjectLink(props) {
+    if (props.link !== "") {
+        return (
+            <a href={props.link}>Learn more.</a>
+        )
+    }
+}
+
+function Projects() {
+    return (
+        <div className="project-section">
+            {project_data.map((data) => (
+                <div key={data.id}>
+                    <div className="project">
+                        <div className="project-description">
+                            <h3>{data.title}</h3>
+                            <p>{data.body}</p>
+                            <ProjectLink link={data.link} />
+                        </div>
+                        <div className="project-media">
+                            <Media media={data.media} filename={data.filename} />
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Projects;
