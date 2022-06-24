787e6cbc044a937dbf24386b302f7042c687e9b7
calhep <callumahepworth@gmail.com>
2022-06-23 22:02:18 -0700

    Updates

---
+++ precache-manifest.0171ca7b7928f5b1616fc1cc29d14940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4ff8ef74e4cdad6a1f",
+    "url": "/static/css/main.9a3f44f3.chunk.css"
+  },
+  {
+    "revision": "0d4ff8ef74e4cdad6a1f",
+    "url": "/static/js/main.0d4ff8ef.chunk.js"
+  },
+  {
+    "revision": "b64cc9ac917ff993e85e",
+    "url": "/static/css/1.da5f7027.chunk.css"
+  },
+  {
+    "revision": "b64cc9ac917ff993e85e",
+    "url": "/static/js/1.b64cc9ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d26a8d4bbb30e5f1536ffa186c47441c",
+    "url": "/index.html"
+  }
+];
