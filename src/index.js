cc9e023950feeaa55e8287ae05bd472fcff2378e
calhep <callumahepworth@gmail.com>
2022-06-24 00:03:17 -0700

    Updated router.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
